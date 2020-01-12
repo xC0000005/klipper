1bb4ee06361620d109dd270c52bcdf338be1b0f9
xC0000005 <32139633+xC0000005@users.noreply.github.com>
2020-01-12 09:51:08 -0800

    Add Lerdge Encryption Functions.

--- scripts/encrypt_firmware.py
+++ scripts/encrypt_firmware.py
@@ -108,14 +108,25 @@ def chituf_encrypt(source, output_directory):
     firmware.close()
     update.close()
 
+def lerdge_encrypt_byte(byte):
+    byte = 0xFF & ((byte << 6) | (byte >> 2))
+    i = 0x58 + byte
+    j = 0x05 + byte + (i >> 8)
+    byte = (0xF8 & i) | (0x07 & j)
+    return byte
+
 # Encrypt ${PROGNAME}.bin and save it as 'update.cbd'
 def lerdge_encrypt(source, output_directory, target):
     firmware = open(source, "rb")
     update = open(output_directory + target, "wb")
-    length = os.path.getsize(target)
+ 
+    input = bytearray(firmware.read())
+    for i in range(len(input)):
+        b = input[i]
+        b = lerdge_encrypt_byte(b)
+        input[i] = b
 
-    # Copy over the lerdge crypt functions
-    #chituf_encrypt_file(firmware, update, length)
+    update.write(input)
 
     firmware.close()
     update.close()
@@ -133,6 +144,6 @@ print "Encrytping " + args.sourceFile + " " + "to " + args.outputDirectory + " f
 if args.targetEncryption == "CHITUF":
     chituf_encrypt(args.sourceFile, args.outputDirectory)
 elif args.targetEncryption == "LERDGEX":
-    lerdge_encrypt(args.sourceFile, args.outputDirectory, '/Lerdge_X_System_Firmware.bin')
+    lerdge_encrypt(args.sourceFile, args.outputDirectory, '/Lerdge_X_firmware_force.bin')
 elif args.targetEncryption == "LERDGEK":
-    lerdge_encrypt(args.sourceFile, args.outputDirectory, '/Lerdge_K_System_Firmware.bin')
+    lerdge_encrypt(args.sourceFile, args.outputDirectory, '/Lerdge_K_firmware_force.bin')
