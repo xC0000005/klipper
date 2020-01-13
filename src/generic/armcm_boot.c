3439ec7e4fd0e52307bc2a7727a4ffb7131a8586
xC0000005 <32139633+xC0000005@users.noreply.github.com>
2020-01-12 20:16:57 -0800

    Disable Interrupts during init for bootloader interaction.

--- src/generic/armcm_boot.c
+++ src/generic/armcm_boot.c
@@ -27,7 +27,7 @@ ResetHandler(void)
 {
     // If there was a bootloader, there may be ISRs running which use
     // Memory addresses to interact with propriatary software. 
-    __disable_irq()
+    __disable_irq();
 
     // Copy global variables from flash to ram
     uint32_t count = (&_data_end - &_data_start) * 4;
